236bfaf3735d3102b3ed26bf06683cc7f7ae33cd
Spencer Hulse <hulse.spencer@gmail.com>
2021-12-23 00:59:24 -0500

    added onclick functionality

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -20,3 +20,12 @@ const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playSound);
+
+function playSoundOnClick(e) {
+  const audio = document.querySelector(`audio[data-key="${e}"]`);
+  const key = document.querySelector(`.key[data-key="${e}"]`);
+  
+  audio.play();
+
+  key.classList.add("playing");
+}
